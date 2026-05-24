fe206e27464d199279969cf3cf39d4ad4fc5fb50
Jovanie Cabatuan <jhovaniecabatuan@gmail.com>
2026-05-24 16:14:21 +0800

    created a bottom footer

---
+++ src/components/BottomFooter.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+// import { CheckBadgeIcon, ShieldCheckIcon, TruckIcon } from '@heroicons/vue/20/solid'
+import { CheckBadgeIcon, ShieldCheckIcon, TruckIcon } from '@heroicons/vue/20/solid'
+import IconHeadset from './icons/IconHeadset.vue'
+import { title } from 'process'
+
+const infos = [
+  {
+    title: '100% Authentic',
+    description: 'we only sell original and high quality products',
+    icon: CheckBadgeIcon,
+  },
+  {
+    title: 'secure payment',
+    description: 'multiple safe payment option available',
+    icon: ShieldCheckIcon,
+  },
+  {
+    title: 'nationwide shipping',
+    description: 'we deliver to any part of the philippines',
+    icon: TruckIcon,
+  },
+  {
+    title: 'customer support',
+    description: 'We are here to help! message us any time',
+    icon: IconHeadset,
+  },
+]
+</script>
+
+<template>
+  <div class="grid grid-cols-2 lg:grid-cols-4 justify-items-center align-items-center gap-2 p-10">
+    <div
+      v-for="(info, index) in infos"
+      :key="index"
+      class="flex flex-row text-white justify-start w-full"
+    >
+      <component :is="info.icon" class="max-w-[40px] text-[#EF4444]" />
+      <div class="flex flex-col justify-center align-center pl-3">
+        <h1 class="uppercase font-bold">{{ info.title }}</h1>
+        <p class="capitalize">{{ info.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
